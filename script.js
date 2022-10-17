663758da84be298732216943c9491b60a7c02166
cata82bn <116027558+cata82bn@users.noreply.github.com>
2022-10-17 21:15:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function afiseazaText() {
+  document.querySelector("#experienta1").classList.toggle("element-ascuns");
+
+  //document.queryselector->   ne returneaza elementul
+  //classlist-> ne ofera
+  //toggle-> (v adauga o clasa in cazul in care nu este in lista, si o va sterge daca este in lista (se aplica doar pentru CLASSLIST)
+}
+
+function afiseazatext2() {
+  document.querySelector("experienta2").classList.toggle("element-ascuns");
+}
